b7b66ddfde490b6c72ac6af0eb5c8ba5f857a0f4
Vitor Garcia <105651529+vgal097@users.noreply.github.com>
2022-05-17 11:16:58 -0300

    Add files via upload

---
+++ Exercicios/Exercicio1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idadeMin, idadeMax;
+
+            Console.WriteLine("Digite a idade do aluno mais novo e a do mais velho: ");
+            idadeMin = int.Parse(Console.ReadLine());
+            idadeMax = int.Parse(Console.ReadLine());
+
+            idadeMin = (idadeMin + idadeMax) / 2;
+
+            Console.WriteLine("A idade media dos alunos é: " + idadeMin + " anos");
+
+        }
+    }
+}
