31d9b2c97e9d5d8723b4237a48f4c19434673bf4
Vitor Garcia <105651529+vgal097@users.noreply.github.com>
2022-05-17 11:19:29 -0300

    Add files via upload

---
+++ Exercicio4/Exercicio4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double temp;
+
+            Console.WriteLine("Digite o valor da temperatura em graus celsius: ");
+            temp = double.Parse(Console.ReadLine());
+
+            temp = (9 * temp + 160) / 5;
+
+            Console.WriteLine("A temperatura é de " + temp + " F");
+
+        }
+    }
+}
