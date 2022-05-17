28e545b864306d70a5780a47178f915ad04d8ccc
Vitor Garcia <105651529+vgal097@users.noreply.github.com>
2022-05-17 11:20:52 -0300

    Add files via upload

---
+++ Exercicio6/Exercicio6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double altura;
+
+            Console.WriteLine("Digite sua altura:");
+            altura = double.Parse(Console.ReadLine());
+
+            altura = (72.7 * altura) - 58;
+
+            Console.WriteLine("Seu peso ideal é de: " + altura.ToString("F1") + " quilos");
+
+        }
+    }
+}
