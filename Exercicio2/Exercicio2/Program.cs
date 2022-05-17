5ebdc04896187af4e8143b99cb321366a70323ec
Vitor Garcia <105651529+vgal097@users.noreply.github.com>
2022-05-17 11:18:02 -0300

    Add files via upload

---
+++ Exercicio2/Exercicio2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double euro;
+
+            Console.WriteLine("Digite o valor em euros:");
+            euro = double.Parse(Console.ReadLine());
+
+            euro *= 5.34;
+
+            Console.WriteLine("o resultado da conversao é de: " + euro + " reais");
+
+        }
+    }
+}
