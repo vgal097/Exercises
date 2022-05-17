e03f17053a9904646de6301af9edf1d7ef4edb48
Vitor Garcia <105651529+vgal097@users.noreply.github.com>
2022-05-17 11:20:09 -0300

    Add files via upload

---
+++ Exercicio5/Exercicio5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double temp;
+
+            Console.WriteLine("Digite o valor da temperatura em graus Fahrenheit: ");
+            temp = double.Parse(Console.ReadLine());
+
+            temp = ((temp - 32) * 5) / 9;
+
+            Console.WriteLine("A temperatura é de " + temp.ToString("F2") + " C");
+
+        }
+    }
+}
