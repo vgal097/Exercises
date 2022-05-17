fb6f951ccbdcb3a6f90080e76e806de8da3279a8
Vitor Garcia <105651529+vgal097@users.noreply.github.com>
2022-05-17 11:18:53 -0300

    Add files via upload

---
+++ Exercicio3/Exercicio3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor;
+
+            Console.WriteLine("Digite o valor mensal de maio referente a venda de carros:");
+            valor = double.Parse(Console.ReadLine());
+
+            valor *= 0.05;
+
+            Console.WriteLine("Sua comissão é de: " + valor + " reais");
+
+        }
+    }
+}
