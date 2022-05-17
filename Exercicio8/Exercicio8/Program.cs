d54b2b44ba658dda1b46631d4223420d04caa3ba
Vitor Garcia <105651529+vgal097@users.noreply.github.com>
2022-05-17 11:21:35 -0300

    Add files via upload

---
+++ Exercicio8/Exercicio8/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b;
+
+            Console.WriteLine("Digite o valor A e o valor B: ");
+            a = double.Parse(Console.ReadLine());
+            b = double.Parse(Console.ReadLine());
+
+            a -= b;
+
+            if (a > 0)
+            {
+                Console.WriteLine("o valor total é: " + a + ", sendo um numero POSITIVO");
+            }
+            else if (a == 0)
+            {
+                Console.WriteLine("o valor total é: " + a + ", sendo um numero NEUTRO");
+            }
+            else
+            {
+                Console.WriteLine("o valor total é: " + a + ", sendo um numero NEGATIVO");
+            }
+        }
+    }
+}
